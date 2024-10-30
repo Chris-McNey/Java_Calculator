ac8671ee312eff1d999c9c1d60cc47cbe92e61ff
Chris <127339191+ChrisandGuts05@users.noreply.github.com>
2024-10-30 15:31:05 -0400

    add percentage functionality and try block

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
+        String firstNum = "";
+        String secondNum;
         double x;
         double y;
         double z = 0;
@@ -18,32 +19,67 @@ public class Main {
         DecimalFormat df = new DecimalFormat("#,###.#######");
 
         do {
-            if (answer.equals("y")) {
-                x = z;
-            } else {
-                System.out.print("Please enter your first number: ");
-                x = Double.parseDouble(scanner.nextLine());
-            }
-            System.out.print("What operation would you like to perform?: ");
-            operator = scanner.nextLine().charAt(0);
-            System.out.print("Please enter your second number: ");
-            y = Double.parseDouble(scanner.nextLine());
-            if (operator == '/' && y == 0) {
-                System.out.println("Cannot divide my zero. \n");
-            } else {
-                z = switch (operator) {
-                    case '+' -> addition(x, y);
-                    case '-' -> subtraction(x, y);
-                    case '*' -> multiplication(x, y);
-                    case '/' -> division(x, y);
-                    default -> z;
-                };
-                xFormat = df.format(x);
-                yFormat = df.format(y);
+            try {
+                if (answer.equals("y")) {
+                    x = z;
+                } else {
+                    System.out.print("Please enter your first number: ");
+                    firstNum = scanner.nextLine();
+                    if (firstNum.endsWith("%")) {
+                        x = Double.parseDouble(firstNum.substring(0, firstNum.length() - 1)) / 100;
+                    } else {
+                        x = Double.parseDouble(firstNum);
+                    }
+                }
+                System.out.print("What operation would you like to perform?: ");
+                operator = scanner.nextLine().charAt(0);
+                System.out.print("Please enter your second number: ");
+                secondNum = scanner.nextLine();
+                if (secondNum.endsWith("%")) {
+                    y = Double.parseDouble(secondNum.substring(0, secondNum.length() - 1)) / 100;
+                } else {
+                    y = Double.parseDouble(secondNum);
+                }
+                if (operator == '/' && y == 0) {
+                    System.out.println("Cannot divide my zero. \n");
+                } else {
+                    z = switch (operator) {
+                        case '+' -> {
+                            if (secondNum.endsWith("%")) {
+                                y *= x;
+                            }
+                            yield addition(x, y);
+                        }
+                        case '-' -> {
+                            if (secondNum.endsWith("%")) {
+                                y *= x;
+                            }
+                            yield subtraction(x, y);
+                        }
+                        case '*', 'x', 'X' -> multiplication(x, y);
+                        case '/' -> division(x, y);
+                        default -> z;
+                    };
+                }
+                if (firstNum.endsWith("%")) {
+                    xFormat = firstNum;
+                } else {
+                    xFormat = df.format(x);
+                }
+                if (secondNum.endsWith("%")) {
+                    yFormat = secondNum;
+                } else {
+                    yFormat = df.format(y);
+                }
                 result = df.format(z);
-                System.out.println(xFormat + " " + operator + " " + yFormat + " = " + result + "\n");
-                System.out.println("Would you like to reuse your answer?(y/n): ");
-                answer = scanner.nextLine();
+                if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+                    System.out.println("That was an invalid operation. Please try again.");
+                } else {
+                    System.out.println(xFormat + " " + operator + " " + yFormat + " = " + result + "\n");
+                    System.out.println("Would you like to reuse your answer?(y/n): ");
+                    answer = scanner.nextLine();
+                }
+            } catch (NumberFormatException ignored) {
             }
         } while (true);
     }
